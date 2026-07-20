ad236b495c628a5890e228f0add38932b0dce3df
Jeff Jung <djung@exaforce.com>
2026-07-21 01:20:33 +0800

    feat(examples): prepend helm repo add/update to install command outputs

--- examples/logsource-aws-eks-all-clusters/outputs.tf
+++ examples/logsource-aws-eks-all-clusters/outputs.tf
@@ -2,12 +2,17 @@ output "helm_install_commands" {
   description = "Run these commands to install the ExaForce agent on each cluster"
   value = {
     for c in data.exaforce_aws_eks_clusters.all.clusters :
-    c.name => join(" \\\n  ", [
-      "helm install exabot-k8s exaforce/exabot-k8s",
-      "--namespace exaforce --create-namespace",
-      "--set exabotK8s.serviceAccount.roleArn=\"${c.exabot_role_arn}\"",
-      "--set exabotK8s.env.queueUrl=\"${c.exabot_sqs_url}\"",
-      "--set exabotK8s.env.configBucketId=\"${c.bucket_id}\"",
+    c.name => join("\n", [
+      "helm repo add exaforce https://exaforce.github.io/helm-charts",
+      "helm repo update",
+      "",
+      join(" \\\n  ", [
+        "helm install exabot-k8s exaforce/exabot-k8s",
+        "--namespace exaforce --create-namespace",
+        "--set exabotK8s.serviceAccount.roleArn=\"${c.exabot_role_arn}\"",
+        "--set exabotK8s.env.queueUrl=\"${c.exabot_sqs_url}\"",
+        "--set exabotK8s.env.configBucketId=\"${c.bucket_id}\"",
+      ]),
     ])
   }
 }
--- examples/logsource-aws-eks-full-automation/outputs.tf
+++ examples/logsource-aws-eks-full-automation/outputs.tf
@@ -5,11 +5,16 @@ output "logsource_name" {
 
 output "helm_install_command" {
   description = "Equivalent helm install command for reference or manual retry"
-  value = join(" \\\n  ", [
-    "helm install exabot-k8s exaforce/exabot-k8s",
-    "--namespace exaforce --create-namespace",
-    "--set exabotK8s.serviceAccount.roleArn=\"${var.exabot_role_arn}\"",
-    "--set exabotK8s.env.queueUrl=\"${var.exabot_sqs_url}\"",
-    "--set exabotK8s.env.configBucketId=\"${var.bucket_id}\"",
+  value = join("\n", [
+    "helm repo add exaforce https://exaforce.github.io/helm-charts",
+    "helm repo update",
+    "",
+    join(" \\\n  ", [
+      "helm install exabot-k8s exaforce/exabot-k8s",
+      "--namespace exaforce --create-namespace",
+      "--set exabotK8s.serviceAccount.roleArn=\"${var.exabot_role_arn}\"",
+      "--set exabotK8s.env.queueUrl=\"${var.exabot_sqs_url}\"",
+      "--set exabotK8s.env.configBucketId=\"${var.bucket_id}\"",
+    ]),
   ])
 }
